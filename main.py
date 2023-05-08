d928587d358ae716a7070a1a39aed482c070bb3d
tdoan1213 <tdoan23@bcp.org>
2023-05-08 11:33:39 -0700

    4th push: built off video game code

--- main.py
+++ main.py
@@ -1,52 +1,56 @@
-# File by Tim Doan
+# File created by: Tim Doan
 
-'''
-Game Overview:
-
-Goals:
-create a working Pong-style game and style it with colors (vertical, maybe horizontal?)
-
-Rules
-Once a player hits the ball, it bounces back to the other side. If the ball goes behind the player,
-then the other player scores a point.
-
-Freedoms
-players are free to move up and down, but not side to side.
-'''
-
-# libraries
+# import libs
 import pygame as pg
 import os
+# import settings 
 from settings import *
 from sprites import *
+# from pg.sprite import Sprite
 
 # set up assets folders
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
-# game 
+# create game class in order to pass properties to the sprites file
+
 class Game:
     def __init__(self):
         # init game window etc.
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("Final Project")
+        pg.display.set_caption("my game")
         self.clock = pg.time.Clock()
         self.running = True
         print(self.screen)
     def new(self):
-    # starting a new game 
+        # starting a new game
         self.score = 0
 
         self.all_sprites = pg.sprite.Group()
-        # self.borders = pg.sprite.Group()
-        # self.ball = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
 
         self.player = Player(self)
+        # self.wall1 = Wall(WIDTH, 50, 0, HEIGHT, (200,250,200), "left")
+        # self.wall2 = Wall(WIDTH, 50, 0, HEIGHT, (200,250, 200), "right")
 
+
+        # self.all_sprites.add(self.wall1)
+        # self.all_sprites.add(self.wall2)
+
+
+        # self.walls.add(self.wall1)
+        # self.walls.add(self.wall2)
+        
         self.all_sprites.add(self.player)
+        
 
+        # for wall in WALL_LIST:
+        #     w = Wall(*wall)
+        #     self.all_sprites.add(w)
+        #     self.walls.add(w)
+        self.run()
     def run(self):
         self.playing = True
         while self.playing:
@@ -61,23 +65,40 @@ class Game:
                 if self.playing:
                     self.playing = False
                 self.running = False
-    
+
     def update(self):
         self.all_sprites.update()
+        if self.player.pos.x < 75:
+            hits = pg.sprite.spritecollide(self.player, self.walls, False)
+            self.player.vel.x = 1
+            print("left wall")
+        if self.player.pos.x > 725:
+            hits = pg.sprite.spritecollide(self.player, self.walls, False)
+            self.player.vel.x = -1
+            print("right wall")
+
 
     def draw(self):
         self.screen.fill(WHITE)
         self.all_sprites.draw(self.screen)
+        # is this a method or a function?
         pg.display.flip()
-
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
     def get_mouse_now(self):
         x,y = pg.mouse.get_pos()
         return (x,y)
 
+# instantiate the game class...
 g = Game()
 
+# kick off the game loop
 while g.running:
     g.new()
 
 pg.quit()
-
--- sprites.py
+++ sprites.py
@@ -1,57 +1,57 @@
-# File by Tim Doan
-
-# libraries
 import pygame as pg
 from pygame.sprite import Sprite
 from settings import *
 from random import randint
 
+
 vec = pg.math.Vector2
 
-# left player class
+# player class
 
 class Player(Sprite):
     def __init__(self, game):
         Sprite.__init__(self)
-        self.game = game 
-        self.image = pg.Surface((25,75))
-        self.image.fill(BLACK)
+        # these are the properties
+        self.game = game
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, HEIGHT/2)
         self.pos = vec(WIDTH/2, HEIGHT/2)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
-        self.confric = 0.1
-# keyboard commands 
+        self.cofric = 0.1
+        self.canjump = False
     def input(self):
         keystate = pg.key.get_pressed()
         if keystate[pg.K_w]:
-            self.acc.y = -PLAYER_ACC
+            self.vel.y = -2
         if keystate[pg.K_s]:
-            self.acc.y = PLAYER_ACC
-
-    def inbounds(self):
-        if self.rect.x > WIDTH:
-            self.pos.x = WIDTH - 25
-            self.vel.x = 0
-            print("i am off the right side of the screen...")
-        if self.rect.x < 0:
-            self.pos.x = 25
-            self.vel.x = 0
-            print("i am off the left side of the screen...")
-        if self.rect.y > HEIGHT:
-            print("i am off the bottom of the screen")
-        if self.rect.y < 0:
-            print("i am off the top of the screen...")
+            self.vel.y = 2
 
     def update(self):
-        self.acc = vec(0, PLAYER_GRAV)
-        self.acc.x = self.vel.x * PLAYER_FRICTION
+        # self.acc = vec(0, PLAYER_GRAV)
+        self.acc.x = self.vel.x
         self.input()
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
         self.rect.midbottom = self.pos
 
+class Mob(Sprite):
+    def __init__(self,width,height, color):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/4, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+    # ...
     def inbounds(self):
         if self.rect.x > WIDTH:
             self.vel.x *= -1
@@ -65,10 +65,22 @@ class Player(Sprite):
         if self.rect.y > HEIGHT:
             self.vel.y *= -1
             self.acc = self.vel * -self.cofric
-
     def update(self):
         self.inbounds()
         # self.pos.x += self.vel.x
         # self.pos.y += self.vel.y
         self.pos += self.vel
         self.rect.center = self.pos
+
+# class Wall(Sprite):
+#     def __init__(self, x, y, width, height, color, variant):
+#         Sprite.__init__(self)
+#         self.width = width
+#         self.height = height
+#         self.image = pg.Surface((self.width,self.height))
+#         self.image.fill(BLACK)
+#         self.rect = self.image.get_rect()
+#         self.rect.x = x
+#         self.rect.y = y
+#         self.variant = variant
+        
